c4f372ec656e55fe79fcdff2bc5cb0d9538a2244
melpgala <93047039+melpgala@users.noreply.github.com>
2023-06-25 12:39:12 -0500

    Update Cobranza.java

--- ProyectoPoo-master/src/main/java/com/mycompany/proyectopoo/Cobranza.java
+++ ProyectoPoo-master/src/main/java/com/mycompany/proyectopoo/Cobranza.java
@@ -3,34 +3,13 @@ package com.mycompany.proyectopoo;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Cobranza extends Personal{
-    
-    public Cobranza(String usuario, String contrasena, String nombre,Cargo cargo){
-        super(usuario,contrasena,nombre,cargo);
-    }
+public class Cobranza implements IPersonal{
     
     @Override
     public void mostrarMenu(){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Seleccione el número de la acción que desea realizar:");
-      System.out.println("1. Generar facturas a empresas.\n2. Reporte de ingresos por servicios.\n3. Reporte de atenciones por técnico.");
-      int opcion = sc.nextInt();
-      sc.nextLine();
-      
-       //verificacion de uso de opcion correcta
-       while(opcion < 1 && opcion > 3){
-           System.out.println("Ingrese el número de una acción valida");
-           opcion = sc.nextInt();
-           sc.nextLine();
-        }
-       
-        // llamando los metodos correspondientes a las opciones
-        switch(opcion){
-          case 1 -> generarFactura();
-          case 2 -> reporteIngreso();
-          case 3 -> reporteTecnico(); 
-        }            
+        // insertar codigo para desplegar menu para Cobranza
     }
+    // Insertar metodos necesarios para  Cobranza
 
     
     public void generarFactura(){
