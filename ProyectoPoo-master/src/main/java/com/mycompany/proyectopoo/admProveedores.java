c4c90fe859a211902b417dd86e777dc07edbe5d9
melpgala <93047039+melpgala@users.noreply.github.com>
2023-06-25 12:31:51 -0500

    Add files via upload

---
+++ ProyectoPoo-master/src/main/java/com/mycompany/proyectopoo/admProveedores.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectopoo;
+
+/**
+ *
+ * @author melan
+ */
+public class admProveedores implements Menu{
+
+    @Override
+    public void mostrarMenu() {
+        // se escribe el codigo para arministrar proveedores
+    }
+    
+}
---
+++ ProyectoPoo-master/src/main/java/com/mycompany/proyectopoo/controladorMenu.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectopoo;
+
+/**
+ *
+ * @author melan
+ */
+public class controladorMenu {
+    
+//    se esscribe el codigo para agregar 
+//    las opciones del menu
+//    se ecribe codigo para mosrtrar el menu al usuario de acuerdo 
+//            a lo seleccionado
+//    
+}
